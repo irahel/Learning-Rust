e4734b26f9e41110e02d89844bb97428a60e3312
Irahel <isaac_rahel@live.com>
2023-02-09 09:37:55 -0300

    feat: learning if

--- if/if.rs
+++ if/if.rs
@@ -1,8 +1,17 @@
 fn main()
 {
-    let age = 25;
-    if age >= 18
+    let age:u8 = 16;
+    let auth:bool = true;
+    let legal_age = age >= 18 || (age >= 16 && auth);
+    if legal_age
     {
         println!("You are old enough to party!");
     }
+    else
+    {
+        println!("You are not old enough to party!");
+    }
+
+    let condition = if legal_age { "of legal" } else { "not of legal" };
+    println!("You are {} age", condition);
 }
