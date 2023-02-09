1004787754f82bb7571244162c0200a9057f467b
Irahel <isaac_rahel@live.com>
2023-02-09 17:23:07 -0300

    feat: add to enum

--- enum/enum.rs
+++ enum/enum.rs
@@ -1,4 +1,4 @@
-enum week
+enum Week
 {
     Monday,
     Tuesday,
@@ -9,11 +9,38 @@ enum week
     Sunday
 }
 
-fn is_weekday(day: week) -> bool
+enum Color
+{
+    Red,
+    Green,
+    Blue,
+    RGB(u8, u8, u8)
+}
+
+fn print_color()
+{
+    let c = Color::RGB(0, 0, 0);
+    println!("Color: {}", match c
+    {
+        Color::Red => "Red",
+        Color::Green => "Green",
+        Color::Blue => "Blue",
+        Color::RGB(0, 0, 0) => "Black",
+        Color::RGB(_, _, _) => "rgb"
+    })
+
+}
+
+fn is_weekday(day: Week) -> bool
 {
     match day
     {
-        week::Saturday | week::Sunday => false,
+        Week::Saturday | Week::Sunday => false,
         _ => true
     }
+}
+
+fn main()
+{
+
 }
