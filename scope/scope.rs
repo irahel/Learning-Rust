e6d7ea8ec87ed0e5a3c0536821811fb4f0c34a07
Irahel <isaac_rahel@live.com>
2023-02-09 09:04:16 -0300

    feat: learning scopes

---
+++ scope/scope.rs
@@ -0,0 +1,18 @@
+fn foo(){
+    let example:i8 = 123;
+    {
+        let example:i8 = -123;
+        println!("foo: {} in", example);
+    }
+    println!("foo: {} out", example);
+}
+
+fn bar(){
+    let example:u8 = 111;
+    println!("bar: {}", example);
+}
+
+fn main() {
+    foo();
+    bar();
+}
