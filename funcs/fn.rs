f9992c46cc96d565f48aae0003d0a4049686352a
Irahel <isaac_rahel@live.com>
2023-02-09 09:12:49 -0300

    feat: learning functions

---
+++ funcs/fn.rs
@@ -0,0 +1,15 @@
+fn sum(a:i32, b:i32) -> i32
+{
+    println!("a: {} + b: {} = {}", a, b, a+b);
+
+    println!("The last expression is the return value, without ;");
+    println!("The keyword return is just for early returns");
+
+    a + b
+}
+
+fn main()
+{
+    println!("By default functions dont return anything");
+    println!("Sum = {}", sum(33, 36));
+}
