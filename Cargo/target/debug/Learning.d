b73381118913bf9d6ada6bf07542f6226697583f
Irahel <isaac_rahel@live.com>
2023-02-09 13:40:13 -0300

    fix: remove cargo build

--- Cargo/target/debug/Learning.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Windows\ 11\Rust\Cargo\target\debug\learning.exe: C:\Users\Windows\ 11\Rust\Cargo\src\main.rs
--- Cargo/target/debug/deps/Learning.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Windows 11\Rust\Cargo\target\debug\deps\learning.exe: src\main.rs
-
-C:\Users\Windows 11\Rust\Cargo\target\debug\deps\learning.d: src\main.rs
-
-src\main.rs:
