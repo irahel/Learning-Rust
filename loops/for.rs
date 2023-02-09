4011f5b1fe3545115e03aa286aed3980385ee4e0
Irahel <isaac_rahel@live.com>
2023-02-09 10:10:38 -0300

    feat: add for

---
+++ loops/for.rs
@@ -0,0 +1,16 @@
+fn main()
+{
+    println!("For walks over an iterator");
+    println!("Left is closed and Right is open");
+    for i in 1..11
+    {
+        println!("i = {}", i);
+    }
+
+    for i2 in 1..=10
+    {
+        println!("i2 = {}", i2);
+    }
+
+
+}
