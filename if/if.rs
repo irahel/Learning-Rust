8ea982191b2c4b8df3aa1251b17b7d18cd7a00d4
Irahel <isaac_rahel@live.com>
2023-02-09 09:39:03 -0300

    doc: add comment

--- if/if.rs
+++ if/if.rs
@@ -12,6 +12,7 @@ fn main()
         println!("You are not old enough to party!");
     }
 
+    println!("IF is an expression, not a statement and can be used to initialize variables");
     let condition = if legal_age { "of legal" } else { "not of legal" };
     println!("You are {} age", condition);
 }
