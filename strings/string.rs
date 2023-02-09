43d67f9be106f2ea68bc2b5c5f70ab5fc162d5ca
Irahel <isaac_rahel@live.com>
2023-02-09 12:35:29 -0300

    feat: add string ref

--- strings/string.rs
+++ strings/string.rs
@@ -3,6 +3,19 @@ fn steals(s:String)
     println!("I stole {}", s);
 }
 
+fn steals_ref(s:&String)
+{
+    println!("References are immutable by default");
+    println!("I not stole {}", s);
+}
+
+fn steals_ref_and_change(s:&mut String)
+{
+    println!("But we can use functions if mut is specified");
+    s.push_str(" is my name");
+    println!("I not stole {}", s);
+}
+
 fn main()
 {
     println!("Stack allocated string");
@@ -19,4 +32,15 @@ fn main()
 
     println!("Values have only one owner");
     println!("Copy values on the heap is not cheap");
+
+    println!("An alternative is to use references");
+    let str_h = String::from("Rahel");
+    steals_ref(&str_h);
+
+    println!("Now we can use the string again");
+    println!("{}", str_h);
+
+    let mut str_h = String::from("Rahel");
+    steals_ref_and_change(&mut str_h);
+    println!("{}", str_h);
 }
