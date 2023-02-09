0f6b4f0a2c95bdb643dcd7f2ce48a50ed2c18715
Irahel <isaac_rahel@live.com>
2023-02-09 13:12:55 -0300

    feat: learning cargo

---
+++ Cargo/target/debug/Learning.d
@@ -0,0 +1,1 @@
+C:\Users\Windows\ 11\Rust\Cargo\target\debug\learning.exe: C:\Users\Windows\ 11\Rust\Cargo\src\main.rs
---
+++ Cargo/target/debug/deps/Learning.d
@@ -0,0 +1,5 @@
+C:\Users\Windows 11\Rust\Cargo\target\debug\deps\learning.exe: src\main.rs
+
+C:\Users\Windows 11\Rust\Cargo\target\debug\deps\learning.d: src\main.rs
+
+src\main.rs:
