8a7140f22d83dac87542e763829930d9e2d459af
Irahel <isaac_rahel@live.com>
2023-02-10 12:53:34 -0300

    fix: fix sintax

--- iflet/iflet.rs
+++ iflet/iflet.rs
@@ -7,7 +7,22 @@ fn main()
 {
     let content = one_or_none();
 
-    if let Some(value) = content
+    if let Some(fn one_or_none() -> Option<i32>
+    {
+        Some(1)
+    }
+    
+    fn main()
+    {
+        let content = one_or_none();
+    
+        if let Some(value) = content
+        {
+            println!("{}", value);
+        }
+    
+        //While let is a loop, if let is a conditional
+    }) = content
     {
         println!("{:?}", content);
     }
